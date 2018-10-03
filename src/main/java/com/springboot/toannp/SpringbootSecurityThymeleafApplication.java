fc8f78b5c4e809cc6f2bd86461318b7030d1d43a
Toan Nguyen <avatar1341991@gmail.com>
2018-10-04 01:29:34 +0700

    update src
    update src

---
+++ src/main/java/com/springboot/toannp/SpringbootSecurityThymeleafApplication.java
@@ -0,0 +1,12 @@
+package com.springboot.toannp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSecurityThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSecurityThymeleafApplication.class, args);
+	}
+}
